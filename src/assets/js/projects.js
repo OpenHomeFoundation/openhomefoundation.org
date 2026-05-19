6385e32ea5c99ba414eaab260e8420b4a404879e
Dan Long <dannylong1988@gmail.com>
2026-05-19 09:52:16 +0000

    Update styles and JS

--- src/assets/js/projects.js
+++ src/assets/js/projects.js
@@ -1,22 +1,21 @@
 import Swiper from "swiper";
-import { Autoplay } from "swiper/modules";
-
-Swiper.use([Autoplay]);
+import { Navigation, Pagination } from 'swiper/modules';
 
 new Swiper(".collab-container", {
-  spaceBetween: 24,
-  centeredSlides: true,
-  slidesPerView: 1.2,
-  enabled: true,
-  loop: true,
-  autoplay: {
-    delay: 0,
-    disableOnInteraction: true,
+  modules: [Navigation, Pagination],
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
-  speed: 12000,
+  spaceBetween: 24,
+  slidesPerView: 1.1,
+  enabled: true,
   breakpoints: {
     768: {
-      slidesPerView: "auto",
+      slidesPerView: 2.2,
+    },
+    992: {
+      slidesPerView: 3.2,
     },
   },
 });
