a6ae6ab2683791cd93eadc2da53ae195cda7cff7
Dan Long <dannylong1988@gmail.com>
2026-07-22 14:41:43 +0000

    Add papers collection and privacy position paper with styling

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,14 @@ module.exports = (eleventyConfig) => {
       .sort((a, b) => b.date - a.date);
   });
 
+  // 	--------------------- Papers Collection ---------------------
+  eleventyConfig.addCollection("papers", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob(["src/documents/papers/*.html", "src/documents/papers/*.md"])
+      .filter((item) => !item.inputPath.includes("index.html"))
+      .sort((a, b) => b.date - a.date);
+  });
+
 
   // 	--------------------- Custom Template Languages ---------------------
   eleventyConfig.addPlugin(require("./config/css-config.js"));
